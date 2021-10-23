269096281179e592eabc01e0107b9b82e783fcb7
Mirza Abdul Awal <awal.mirza2016@gmail.com>
2021-10-22 22:44:14 -0400

    Unit Test Nav done

--- src/components/About/__test__/index.test.js
+++ src/components/About/__test__/index.test.js
@@ -17,8 +17,9 @@ describe('About component', () => {
       // Second Test
       it('matches snapshot DOM node structure', () => {
             // render About
+            const { asFragment } = render(<About />);
+            expect(asFragment()).toMatchSnapshot();
       })
-      const { asFragment } = render(<About />);
-      expect(asFragment()).toMatchSnapshot();
+
 
 });
